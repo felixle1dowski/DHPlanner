bbda94ad877d5ce09f97274210b949941b6ecf2d
Felix <felix.david.lewandowski@gmail.com>
2024-11-14 17:01:31 +0100

    main isn't needed anymore.

--- main.py
+++
@@ -1,9 +0,0 @@
-from config import Config
-from logger import Logger
-from dhc_creation_pipeline_factory import DHCCreationPipelineFactory
-
-config = Config()
-logger = Logger()
-Logger.info("Starting...")
-pipeline = DHCCreationPipelineFactory().create_pipeline()
-pipeline.start()
