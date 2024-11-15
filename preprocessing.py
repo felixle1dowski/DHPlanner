78e7100707b726d5c1eb7586b0cca1faaf4925e1
Felix <felix.david.lewandowski@gmail.com>
2024-11-15 14:40:23 +0100

    Added Centroid Identification of buildings to preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,13 @@
 from PyQt5.QtCore import QVariant
-from qgis._core import QgsField
-
 from .config import Config
 from qgis.core import (QgsProject, QgsSpatialIndex, QgsFeatureRequest, QgsVectorLayer,
                        QgsCoordinateReferenceSystem, QgsCoordinateTransform,
-                       QgsMessageLog, Qgis)
+                       QgsMessageLog, Qgis, QgsField, QgsFeature)
 from qgis import processing
-
-
 from .logger import Logger
 
-class Preprocessing:
 
+class Preprocessing:
     DESIRED_CRS = QgsCoordinateReferenceSystem('EPSG:4839')
 
     selection_layer = None
@@ -20,16 +16,20 @@ class Preprocessing:
     buildings_layer_path = None
     buildings_layer = None
     selected_roads_exploded = None
+    buildings_centroids = None
+    preprocessing_result = None
 
     def __init__(self):
         pass
+
+    """Central method for preprocessing. Will start - and finish - the preprocessing pipeline."""
 
     def start(self):
         self.selection_layer = None
         # ToDo: Add back in, when we use files again instead of layers.
         # self.roads_layer_path = Config().get_roads_path()
         # self.buildings_layer_path = Config().get_buildings_path()
-        self.__verify_layer(Config().get_selection_layer_name())
+        self.__verify_layer(Config().get_selection_layer_name(), True)
         self.selection_layer = QgsProject.instance().mapLayersByName(Config().get_selection_layer_name())[0]
         self.__verify_layer(Config().get_roads_layer_name())
         self.roads_layer = QgsProject.instance().mapLayersByName(Config().get_roads_layer_name())[0]
@@ -40,15 +40,32 @@ class Preprocessing:
         self.__select_features(self.roads_layer, self.selection_layer)
         self.__select_features(self.buildings_layer, self.selection_layer)
 
+        # ToDo: This is not good practice. I should not do this in place.
+        # ToDo: I want to change this, so that I only work with temporary layers from the preprocessing stage onward.
+        self.__verify_layer(Config().get_roads_layer_name(), True)
         self.__explode_road_lines()
-        self.__measure_length()
+        self.__measure_lengths_of_roads()
+        self.__assign_ids_to(self.selected_roads_exploded)
+        Logger().info("Roads have been preprocessed successfully.")
+        self.__find_centroids_of_buildings()
+        self.__assign_ids_to(self.buildings_centroids)
+        Logger().info("Buildings have been preprocessed successfully.")
 
-    # needs to be tested.
-    def __verify_layer(self, layer_name):
+    # ToDo: needs to be tested.
+    def __verify_layer(self, layer_name, verify_crs=False):
+        """
+        Verifies the layer for further processing.
+
+        :param layer_name: name of the layer inside the project.
+        :param verify_crs: whether or not to verify the crs.
+                don't verify it right away, when you
+                want to reduce the size of the layer
+                in a later preprocessing step. eg. explode the roads.
+        """
         layer = QgsProject.instance().mapLayersByName(layer_name)[0]
         if layer is None:
             raise Exception(f"No layer with name {layer_name} found.")
-        if layer.crs() != self.DESIRED_CRS:
+        if verify_crs and layer.crs() != self.DESIRED_CRS:
             Logger().warning(f"Layer {layer_name} has the wrong CRS."
                              "It is: {layer.crs()} The right"
                              "CRS is necessary in order to calculate pipe"
@@ -71,6 +88,7 @@ class Preprocessing:
         Logger().info("Selection CRS has been changed successfully.")
 
     def __select_features(self, target_layer, selection_layer):
+        """Selects the features of the target_layer within the selection_layer. Uses Intersection."""
         if not target_layer.isEditable():
             target_layer.startEditing()
         # remove previous selection
@@ -92,6 +110,9 @@ class Preprocessing:
 
     # ToDo: can we generalize this? Not only for roads, but for lines in general?
     def __explode_road_lines(self):
+        """Explodes the road lines. Two purposes:
+        1) Make MultiLine Layer to Line Layer.
+        2) Reduce complexity of vectors."""
         if not self.roads_layer.isValid():
             raise Exception(f"Target layer {self.roads_layer.name()} is invalid.")
         selected_features = self.roads_layer.selectedFeatures()
@@ -99,12 +120,13 @@ class Preprocessing:
             raise Exception(f"No features selected in {self.roads_layer.name()}.")
         # We're creating a copy of the roads layer with only the selected roads
         # By doing so, we can then explode it to obtain only Lines.
-        selected_roads = QgsVectorLayer(f'MultiLineString?crs={self.DESIRED_CRS}', 'selected_roads', 'memory')
+        selected_roads = QgsVectorLayer(f'MultiLineString?crs={self.DESIRED_CRS}',
+                                        'selected_roads',
+                                        'memory')
         selected_roads_data_provider = selected_roads.dataProvider()
         # We're copying the fields from the target layer to the selected layer.
         selected_roads_data_provider.addAttributes(self.roads_layer.fields())
         selected_roads.updateFields()
-
         selected_roads_data_provider.addFeatures(selected_features)
         params = {
             'INPUT': selected_roads,
@@ -115,7 +137,9 @@ class Preprocessing:
         QgsProject.instance().addMapLayer(self.selected_roads_exploded)
         Logger().info("Selected MultiLineStrings exploded successfully.")
 
-    def __measure_length(self):
+    # ToDo: Generalize this for lengths in general.
+    def __measure_lengths_of_roads(self):
+        """Creates dedicated length field for each feature."""
         self.selected_roads_exploded.startEditing()
         self.selected_roads_exploded.dataProvider().addAttributes([QgsField("length", QVariant.Double)])
         self.selected_roads_exploded.updateFields()
@@ -126,3 +150,33 @@ class Preprocessing:
             self.selected_roads_exploded.updateFeature(feature)
         self.selected_roads_exploded.commitChanges()
         Logger().info("Length of selected roads measured successfully.")
+
+    def __assign_ids_to(self, layer):
+        """Assigns unique IDs."""
+        layer.startEditing()
+        layer.dataProvider().addAttributes([QgsField("id", QVariant.Int)])
+        layer.updateFields()
+        for feature in layer.getFeatures():
+            feature.setAttribute("id", feature.id())
+            layer.updateFeature(feature)
+
+    def __find_centroids_of_buildings(self):
+        # Create new temporary layer. ToDo: Do this earlier!
+        selected_features = self.buildings_layer.selectedFeatures()
+        self.buildings_centroids = QgsVectorLayer(f'Point?crs={self.DESIRED_CRS}',
+                                            'selected_building_centroids',
+                                            'memory')
+        building_centroids_data_provider = self.buildings_centroids.dataProvider()
+
+        new_features = []
+        for feature in selected_features:
+            geom = feature.geometry()
+            centroid_geom = geom.centroid()
+            centroid_feature = QgsFeature()
+            centroid_feature.setGeometry(centroid_geom)
+            centroid_feature.setAttributes([feature.id()])
+            new_features.append(centroid_feature)
+
+        building_centroids_data_provider.addFeatures(new_features)
+        self.buildings_centroids.updateExtents()
+        QgsProject.instance().addMapLayer(self.buildings_centroids)
