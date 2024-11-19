24327a5988231837181b4d684452d4057dd68537
FelixLe1dowski <felix.lewandowski@haw-hamburg.de>
2024-11-19 17:51:51 +0100

    Splitting works now. Still needs attributes for the roads!

--- src/multi_step_pipeline/graph_creator.py
+++ src/multi_step_pipeline/graph_creator.py
@@ -178,27 +178,12 @@ class GraphCreator:
         self.building_centroids.commitChanges()
 
     def __add_access_points_to_roads_layer(self, access_points):
-        """Adds the access points to the roads layer.
-        For that it splits the layer by the access points."""
         roads = self.exploded_roads
         roads_provider = roads.dataProvider()
         access_points_features = access_points.getFeatures()
         road_split_points = {}
-        road_split_lines = {}
         road_id_idx = access_points.fields().indexFromName('road_id')
         roads.startEditing()
-
-        # adding a layer to store perpendicular lines in that are used to split
-        # the roads by the access points.
-        perpendicular_lines_layer = QgsVectorLayer(f'LineString?crs=EPSG:4839',
-                                             'PerpendicularLines',
-                                             'memory') # TODO: crs is only set statically for debugging
-        perp_provider = perpendicular_lines_layer.dataProvider()
-        perp_provider.addAttributes([QgsField('id', QVariant.Int)])
-        perp_provider.addAttributes([QgsField('ap_id', QVariant.Int)])
-        perpendicular_lines_layer.updateFields()
-        QgsProject.instance().addMapLayer(perpendicular_lines_layer)
-
         for access_point in access_points_features:
             road_id = access_point.attributes()[road_id_idx]
             Logger().debug(f"Adding road with road id {road_id}")
@@ -207,80 +192,41 @@ class GraphCreator:
             road_split_points[road_id].append(access_point)
             Logger().debug(f"Adding point with point id {access_point.id()}")
         for road_id, p in road_split_points.items():
-            for point in p:
-                line_from_point = self.__create_perpendicular_line_to_line_from_point(perpendicular_lines_layer,
-                                                                    roads, road_id,
-                                                                    access_points,
-                                                                    point.id())
-                if road_id not in road_split_lines:
-                    road_split_lines[road_id] = []
-                road_split_lines[road_id].append(line_from_point)
-        for road_id, l in road_split_lines.items():
+            p_dict = {}
+            reconnection_list = []
             road = roads.getFeature(road_id)
-            # convert lines to points:
-            for line in l:
-                line_poly = line.asPolyline()
-                start_point_of_line = line_poly[0]
-                end_point_of_line = line_poly[-1]
-                points = [start_point_of_line, end_point_of_line]
-                result, new_geometries, point_xy = road.geometry().splitGeometry(points, False, True)
+            road_line = road.geometry().asPolyline()
+            road_start = road_line[0]
+            road_end = road_line[-1]
+            if len(p) > 1:
+                for point in p:
+                    point_x = point.geometry().asPoint().x()
+                    point_y = point.geometry().asPoint().y()
+                    dx = abs(point_x - road_start.x())
+                    dy = abs(point_y - road_start.y())
+                    distance = dx + dy
+                    p_dict[point.id()] = distance
+            if len(p_dict) > 0:
+                dict(sorted(p_dict.items()))
+            reconnection_list.append(road_start)
+            if len(p) == 1:
+                point = p[0]
+                pxy = QgsPointXY(point.geometry().asPoint().x(), point.geometry().asPoint().y())
+                reconnection_list.append(pxy)
+            for p_id in p_dict.keys():
+                point = access_points.getFeature(p_id)
+                pxy = QgsPointXY(point.geometry().asPoint().x(), point.geometry().asPoint().y())
+                reconnection_list.append(pxy)
+            reconnection_list.append(road_end)
+            roads_provider.deleteFeatures([road_id])
+            for p in range(len(reconnection_list) - 1):
+                point1 = reconnection_list[p]
+                point2 = reconnection_list[p + 1]
+                line_geometry = QgsGeometry.fromPolylineXY([point1, point2])
+                feature = QgsFeature()
+                feature.setGeometry(line_geometry)
+                roads_provider.addFeature(feature)
         roads.commitChanges()
-
-
-    # ToDo: Is it even needed to have the lines in a layer? Only useful for debugging here, can probably deleted later on.
-    def __create_perpendicular_line_to_line_from_point(self, line_layer_with_id_field,
-                                                       line_layer, line_id,
-                                                       point_layer, point_id):
-        """Creates perpendicular lines from a point. Only works with line layers,
-        not multi-line."""
-
-        """This is sadly necessary in order to obtain split lines from points.
-        It's not possible to split a line by a point, since the split function
-        does not incorporate a tolerance feature."""
-
-        line_layer_with_id_field.startEditing()
-        provider = line_layer_with_id_field.dataProvider()
-
-        line_to_reference = line_layer.getFeature(line_id)
-        point_to_draw_line_from = point_layer.getFeature(point_id)
-        Logger().debug(f"Attempting to create perpendicular line from point with id {point_to_draw_line_from.id()},"
-                       f"line reference: {line_to_reference.id()}")
-
-        line_geometry = line_to_reference.geometry()
-        line = line_geometry.asPolyline()
-
-        line_start = line[0]
-        line_ending = line[-1]
-        line_angle = self.__calculate_angle_of_line(line_start, line_ending)
-        point_coord = point_to_draw_line_from.geometry().asPoint()
-        center_x = point_coord.x()
-        center_y = point_coord.y()
-        perpendicular_angle = line_angle + math.pi / 2
-        x_offset = self.PERP_LINE_LENGTH * math.cos(perpendicular_angle)
-        y_offset = self.PERP_LINE_LENGTH * math.sin(perpendicular_angle)
-
-        start_point = QgsPoint(center_x - x_offset / 2,
-                                center_y - y_offset / 2)
-        end_point = QgsPoint(center_x + x_offset / 2,
-                                 center_y + y_offset / 2)
-
-        perpendicular_line = QgsGeometry.fromPolyline([start_point, end_point])
-
-        new_feature = QgsFeature(line_layer_with_id_field.fields())
-        DhpUtility().assign_unique_id(line_layer_with_id_field, new_feature, 'id')
-        ap_id = point_to_draw_line_from.id()
-        DhpUtility().assign_value_to_field(line_layer_with_id_field, 'ap_id', new_feature, ap_id)
-        line_layer_with_id_field.updateFields()
-        new_feature.setGeometry(perpendicular_line)
-        provider.addFeature(new_feature)
-        line_layer_with_id_field.commitChanges()
-        return perpendicular_line
-
-    def __calculate_angle_of_line(self, line_start: QgsPointXY, line_ending: QgsPointXY):
-        dx = line_start.x() - line_ending.x()
-        dy = line_start.y() - line_ending.y()
-        angle = math.atan2(dy, dx)
-        return angle
 
     def __split_roads_with_point(self):
         """Splits road by a point."""
